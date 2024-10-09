1cc959ecabc84fe5adbf061f5597350339f3299a
joaorolim <pjrolim@yahoo.com.br>
2024-10-09 05:26:23 -0300

    First commit

---
+++ src/main/java/one/digitalinnovation/gof/controller/ClienteRestController.java
@@ -0,0 +1,69 @@
+package one.digitalinnovation.gof.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import one.digitalinnovation.gof.model.Cliente;
+import one.digitalinnovation.gof.service.ClienteService;
+
+import javax.persistence.EntityNotFoundException;
+
+/**
+ * Esse {@link RestController} representa nossa <b>Facade</b>, pois abstrai toda
+ * a complexidade de integrações (Banco de Dados H2 e API do ViaCEP) em uma
+ * interface simples e coesa (API REST).
+ * 
+ * @author falvojr
+ */
+@RestController
+@RequestMapping("clientes")
+public class ClienteRestController {
+
+	@Autowired
+	private ClienteService clienteService;
+
+	@GetMapping
+	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
+		return ResponseEntity.ok(clienteService.buscarTodos());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Object> buscarPorId(@PathVariable Long id) {
+//		return ResponseEntity.ok(clienteService.buscarPorId(id));
+		try {
+			Cliente cliente = clienteService.buscarPorId(id);
+			return ResponseEntity.ok(cliente);
+		} catch (EntityNotFoundException e) {
+			// Retornar um 404 Not Found com a mensagem da exceção
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
+		System.out.println(cliente);
+		clienteService.inserir(cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		clienteService.atualizar(id, cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id) {
+		clienteService.deletar(id);
+		return ResponseEntity.ok().build();
+	}
+}
